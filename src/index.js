47cde357bf8f3f5f878a179bb6bba1e486a81d24
YoungBrick <tidorka@gmail.com>
2018-09-27 21:46:51 +0300

    solved zerros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let result = 0;
+  for (let i = 5; i <= number; i *= 5) {
+    result += Math.floor(number / i);
+  }
+  return result;
 }
